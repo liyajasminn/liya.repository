0e57dd4a269d3d913276d1359a55c123328760fb
Liya jasmine <liyajasmine261@gmail.com>
2025-12-10 12:29:39 +0530

    work.py mod

--- work.py
+++ work.py
@@ -5,3 +5,9 @@ def add(x,y):
     return x+y
 result = add(5, 3)
 print("The sum is:", result)
+
+
+def subtract_numbers(a, b):
+    return a - b
+result = subtract_numbers(10, 4)
+print("The difference is:", result) 
