ca44a224e879488e6ec5af28781d1e63787274e0
Liya jasmine <liyajasmine261@gmail.com>
2025-12-12 10:55:18 +0530

    work.py mody

--- work.py
+++ work.py
@@ -10,4 +10,14 @@ print("The sum is:", result)
 def subtract_numbers(a, b):
     return a - b
 result = subtract_numbers(10, 4)
-print("The difference is:", result) 
+print("The difference is:", result)  
+
+def div(x,y)
+    return x/y
+result = divide_numbers(10, 4)
+print(result)  
+
+def mul(x,y)
+   return x*y
+result = multiply_numbers(10, 4)
+print(result)  
